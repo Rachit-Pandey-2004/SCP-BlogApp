973e438716336a36aedb1e8ae7382af5a23b11e9
Rachit-Pandey-2004 <rachit_pandey2004@yahoo.com>
2024-03-02 11:36:36 +0530

    added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
-
+import { BrowserRouter ,Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Project from './pages/Project'
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>app</div>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/project' element={<Project/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
