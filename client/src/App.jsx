0b4e3fdf667140b7bac2778a539c0aec14a41de4
Rachit-Pandey-2004 <rachit_pandey2004@yahoo.com>
2024-03-02 12:30:31 +0530

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Project from './pages/Project'
+import Header from './components/Header'
 export default function App() {
   return (
     
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
