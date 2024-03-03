a640a9fe818d7767f626cac529c830b80258bb63
Rachit-Pandey-2004 <rachit_pandey2004@yahoo.com>
2024-03-03 23:34:04 +0530

    redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link ,useNavigate} from 'react-router-dom'
+import { useDispatch,useSelector } from 'react-redux';
+import { signInSuccess,signInFailure,signInStart } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData,setFormData]=useState({});
-  const[errorMessage,setErrorMessage]=useState(null);
-  const[loading,setLoading]=useState(false);
+  const {loading,error:errorMessage}=useSelector(state=>state.user);
+  const dispatch =useDispatch()
   const navigate =useNavigate();
   const handleChange = (e)=>{
     setFormData({...formData,[e.target.id]:e.target.value.trim()});
@@ -13,11 +15,10 @@ export default function SignIn() {
   const handleSubmit=async(e)=>{
     e.preventDefault();
     if(!formData.password||!formData.email){
-      return setErrorMessage('Please fill out all fields !!')
+      return dispatch(signInFailure('Please fill out all fields !!'));
     }
     try{
-      setErrorMessage(null);
-      setLoading(true);
+      dispatch(signInStart());
 
       const res=await fetch('/api/auth/signin',{
         method:'POST',
@@ -28,15 +29,14 @@ export default function SignIn() {
       });
       const data=await res.json();
       if(data.success===false){
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
       if(res.ok){
+        dispatch(signInSuccess(data));
         navigate('/')
       }
     }catch(error){
-      setLoading(false);
-      setErrorMessage(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
@@ -80,7 +80,7 @@ export default function SignIn() {
                   <Spinner size='sm'/>
                   <span className='pl-3'>Loading...</span>
                   </>
-                ): 'Sign Up'
+                ): 'Sign In'
               }
             </Button>
           </form>
