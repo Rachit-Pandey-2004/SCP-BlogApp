96991d5fb37a76fa7a7401778690ab5c3f4a4280
Rachit-Pandey-2004 <rachit_pandey2004@yahoo.com>
2024-03-06 12:44:46 +0530

    Completed profile page functionality bug~google oauth accounts have issue

--- api/index.js
+++ api/index.js
@@ -1,39 +1,36 @@
 import express from 'express';
-import mongoose  from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO
-).then(
-    ()=>{
-        console.log('MongoDB is connected');
-    }
-).catch((err)=>{
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+const app = express();
 
-
-const app=express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3080,()=>{
-    console.log('Server is running on port 3080!!');
+app.listen(3080, () => {
+  console.log('Server is running on port 3080!');
 });
-
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode||500;
-    const message =err.message||'Internal Server Error';
-    res.status(statusCode).json({
-        success:false,
-        statusCode,
-        message
-    })
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
